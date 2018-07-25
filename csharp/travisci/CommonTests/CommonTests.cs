a04029b759a9438c56d2d8893942ddd9457d5d5b
mamori017 <mamori017@outlook.jp>
2018-07-25 16:47:36 +0900

    test

---
+++ csharp/travisci/CommonTests/CommonTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System;
+using System.IO;
+
+namespace CommonTests
+{
+    public class CommonTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}

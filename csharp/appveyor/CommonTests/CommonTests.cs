57d424342d1433d63d6a6b4228c90a89600863e4
mamori017 <mamori017@outlook.jp>
2018-07-25 16:26:23 +0900

    test

---
+++ csharp/appveyor/CommonTests/CommonTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CommonTests
+{
+    [TestClass()]
+    public class CommonTests
+    {
+        [TestMethod()]
+        public void Test()
+        {
+            Assert.AreEqual(1, 1);
+        }
+   }
+}
---
+++ csharp/appveyor/xUnitTest/xUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace XunitTests
+{
+    [TestFixture]
+    public class XunitTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1,Is.EqualTo(1));
+        }
+    }
+}
